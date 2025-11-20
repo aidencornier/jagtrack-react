e687edf5f4cf81b2a813de87a66f112b4fd273fb
Aiden Cornier <Corniera@JEFFERSON.LOCAL>
2025-11-20 12:56:36 -0700

    Checkpoint 2: Components and props with responsive grid

---
+++ src/data/classes.js
@@ -0,0 +1,38 @@
+export const classes = [
+ {
+   id: 1,
+   block: 'A Block',
+   name: 'Chemistry',
+   time: '8:25 AM - 9:55 AM',
+   description: 'Learning all about Chemistry',
+   canvasUrl: 'https://jajags.instructure.com/courses/3338',
+   color: '#e03131'
+ },
+ {
+   id: 2,
+   block: 'B Block',
+   name: 'JavaScript Programming',
+   time: '10:00 AM - 11:30 AM',
+   description: 'Learn to build modern, responsive websites and interactive web applications using React.',
+   canvasUrl: 'https://jajags.instructure.com/courses/3439',
+   color: '#1a5490'
+ },
+ {
+   id: 3,
+   block: 'C Block',
+   name: 'Algebra II',
+   time: '12:15 AM - 1:45 PM',
+   description: 'Master advanced algebraic concepts including functions, polynomials, exponentials, and logarithms.',
+   canvasUrl: '#',
+   color: '#2f9e44'
+ },
+ {
+   id: 4,
+   block: 'D Block',
+   name: 'Biology',
+   time: '1:50 PM - 3:20 PM',
+   description: 'Investigate living systems from cells to ecosystems through hands-on lab work.',
+   canvasUrl: '#',
+   color: '#f08c00'
+ }
+]
